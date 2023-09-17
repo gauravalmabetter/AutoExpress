cb107e8c64a24c8de15b4bb6f0b0cc09ebe640ff
Gaurav verma <133861631+gauravalmabetter@users.noreply.github.com>
2023-09-17 23:32:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -65,11 +65,11 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
+if ("production" === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
